2bc5c0e96019f3b98e282d071eb5d76c316f6295
Hossam-Elgmmal <109397447+Hossam-Elgmmal@users.noreply.github.com>
2024-04-07 07:10:19 +0200

    finished drawing boxes using onTouchEvent(MotionEvent): Boolean

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/Box.kt
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.draganddraw
+
+import android.graphics.PointF
+
+data class Box(val start: PointF) {
+    var end = start
+    val left: Float
+        get() = start.x.coerceAtMost(end.x)
+    val right: Float
+        get() = start.x.coerceAtLeast(end.x)
+    val top: Float
+        get() = start.y.coerceAtMost(end.y)
+    val bottom: Float
+        get() = start.y.coerceAtLeast(end.y)
+
+}
